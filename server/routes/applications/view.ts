33f7d0716ff991c3a7650c3ceca798c63a35d87a
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2026-04-09 13:47:10 +0100

    adding audit service to view apps page

--- server/routes/applications/view.ts
+++ server/routes/applications/view.ts
@@ -1,16 +1,20 @@
 import { Request, Response, Router } from 'express'
-import AuditService from '../../services/auditService'
+import AuditService, { Page } from '../../services/auditService'
 
 import { URLS } from '../../constants/urls'
 import { PATHS } from '../../constants/paths'
 
 import { getPaginationData } from '../../utils/http/pagination'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function viewAppsRouter({ auditService }: { auditService: AuditService }): Router {
   const router = Router()
 
   router.get(URLS.APPLICATIONS, async (req: Request, res: Response) => {
+    await auditService.logPageView(Page.VIEW_APPLICATIONS_PAGE, {
+      who: res.locals.user.username,
+      correlationId: req.id,
+    })
+
     res.render(PATHS.APPLICATIONS.LIST, {
       pagination: getPaginationData(Number(req.query.page) || 1, 20),
       query: req.query,
--- server/services/auditService.ts
+++ server/services/auditService.ts
@@ -2,6 +2,7 @@ import HmppsAuditClient, { AuditEvent } from '../data/hmppsAuditClient'
 
 export enum Page {
   EXAMPLE_PAGE = 'EXAMPLE_PAGE',
+  VIEW_APPLICATIONS_PAGE = 'VIEW_APPLICATIONS_PAGE',
 }
 
 export interface PageViewEventDetails {
