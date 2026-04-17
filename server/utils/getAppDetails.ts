5778741c254324c4e98ed243873ceb4325366fcf
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2026-04-17 14:39:54 +0100

    remove-pin-phone-template

--- server/utils/getAppDetails.ts
+++ server/utils/getAppDetails.ts
@@ -1,6 +1,8 @@
 import { App } from '../@types/managingAppsApi'
 import { AppTypeData } from './getAppTypeLogDetails'
 
+type RemoveContactRequest = Partial<Extract<AppTypeData, { type: 3 }>>
+
 export default async function getApplicationDetails(
   applicationDetails: AppTypeData,
   application?: App,
@@ -13,7 +15,37 @@ export default async function getApplicationDetails(
     return { details }
   }
 
+  const isValid = (value: unknown): boolean =>
+    value !== undefined && value !== null && !(typeof value === 'string' && value.trim() === '')
+
+  const getFallbackValue = <FormType, RequestType, K extends keyof FormType & keyof RequestType>(
+    field: K,
+    form: FormType,
+    request: RequestType,
+    defaultValue: FormType[K],
+  ): FormType[K] | RequestType[K] => {
+    const formValue = form[field]
+    const requestValue = request[field]
+
+    if (isValid(formValue)) return formValue
+    if (isValid(requestValue)) return requestValue
+    return defaultValue
+  }
+
   switch (applicationDetails.type) {
+    case 3: {
+      const request = (application?.requests?.[0] as RemoveContactRequest) ?? {}
+
+      const prefilledDetails = {
+        firstName: getFallbackValue('firstName', applicationDetails, request, ''),
+        lastName: getFallbackValue('lastName', applicationDetails, request, ''),
+        telephone1: getFallbackValue('telephone1', applicationDetails, request, ''),
+        telephone2: getFallbackValue('telephone2', applicationDetails, request, ''),
+        relationship: getFallbackValue('relationship', applicationDetails, request, ''),
+      }
+
+      return prefilledDetails
+    }
     case 5: {
       const { amount, reason } = application?.requests?.[0] ?? {}
       return {
