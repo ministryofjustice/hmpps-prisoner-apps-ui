fcf8c2137915672d51b051d2c496d1b47d05ff77
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2026-04-21 15:26:15 +0100

    add social pin phone contact template

--- server/routes/applications/appDetails.ts
+++ server/routes/applications/appDetails.ts
@@ -6,24 +6,49 @@ import { URLS } from '../../constants/urls'
 import getApplicationDetails from '../../utils/getAppDetails'
 
 import AuditService, { Page } from '../../services/auditService'
+import ManagingAppsService from '../../services/managingAppsService'
+
 import { getAppTypeLogDetailsData } from '../../utils/getAppTypeLogDetails'
 import PersonalRelationshipsService from '../../services/personalRelationshipsService'
 
 export default function appDetailsRouter({
   auditService,
+  managingAppsService,
   personalRelationshipsService,
 }: {
   auditService: AuditService
+   managingAppsService: ManagingAppsService
   personalRelationshipsService: PersonalRelationshipsService
 }): Router {
   const router = Router()
 
   router.get(URLS.LOG_APPLICATION_DETAILS, async (req: Request, res: Response) => {
+    const { user } = res.locals
     const { applicationData } = req.session
 
-    const logDetails = getAppTypeLogDetailsData(undefined, applicationData?.additionalData || {}, undefined)
+    const groups = await managingAppsService.getGroupsAndTypes(user.userId)
 
-    const templateFields = await getApplicationDetails(logDetails, personalRelationshipsService, undefined, undefined)
+    const selectedGroup = groups.find(group => group.id.toString() === applicationData.group?.value)
+    if (!selectedGroup) {
+      return res.redirect(URLS.LOG_GROUP)
+    }
+
+    const selectedAppType = selectedGroup.appTypes.find(type => type.id.toString() === applicationData.type?.value)
+
+    if (!selectedAppType) {
+      return res.redirect(URLS.LOG_GROUP)
+    }
+
+    const isGeneric = selectedAppType.genericType || selectedAppType.genericForm
+
+    const logDetails = getAppTypeLogDetailsData(selectedAppType.id, applicationData?.additionalData || {}, isGeneric)
+
+    const templateFields = await getApplicationDetails(
+      logDetails,
+      personalRelationshipsService,
+      undefined,
+      applicationData.earlyDaysCentre,
+    )
 
     await auditService.logPageView(Page.VIEW_APPLICATIONS_PAGE, {
       who: res.locals.user.username,
@@ -34,6 +59,7 @@ export default function appDetailsRouter({
       ...templateFields,
       applicationType: applicationData?.type,
       title: 'Log details',
+      isGeneric,
     })
   })
 
