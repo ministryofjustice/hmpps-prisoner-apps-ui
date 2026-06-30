ba55a2a1c8222746df5057b72440c5a8d0fe0fdd
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2026-06-30 15:14:09 +0100

    referring to prod env

--- server/constants/activeAgencies.ts
+++ server/constants/activeAgencies.ts
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === 'PROD'
+const isProd = process.env.ENVIRONMENT_NAME === 'PROD'
 
 // eslint-disable-next-line import/prefer-default-export
 export const ACTIVE_AGENCIES: readonly string[] = isProd ? ['RNI'] : ['CKI', 'WLI']
