f660c96311829b4b3a99640cddde8dc5957447fd
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2026-06-30 14:39:59 +0100

    adding ENV var

--- server/constants/activeAgencies.ts
+++ server/constants/activeAgencies.ts
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'PROD'
 
 // eslint-disable-next-line import/prefer-default-export
 export const ACTIVE_AGENCIES: readonly string[] = isProd ? ['RNI'] : ['CKI', 'WLI']
